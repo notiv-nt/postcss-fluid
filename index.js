4673ef2c551f1c95e07fb526a7adf1fa835986f0
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2021-01-20 22:55:38 +0300

    Update version

--- index.js
+++ index.js
@@ -1,60 +1,67 @@
 const postcss = require('postcss');
 
+const PLUGIN_NAME = 'postcss-transition';
+
 const pi = (o) => parseInt(o, 10);
 const pf = (o) => parseFloat(o);
 
-module.exports = postcss.plugin('postcss-transition', (opts) => (root) => {
-  const $opts = Object.assign(
-    {
-      min: '320px',
-      max: '1200px',
-      functionName: 'fluid',
-    },
-    opts
-  );
-  const regex = new RegExp(`${$opts.functionName}\\(([^)]+)\\)`, 'gi');
+module.exports = (opts = {}) => {
+  return {
+    postcssPlugin: PLUGIN_NAME,
+    Once(root) {
+      const $opts = Object.assign(
+        {
+          min: '320px',
+          max: '1200px',
+          functionName: 'fluid',
+        },
+        opts
+      );
+      const regex = new RegExp(`${$opts.functionName}\\(([^)]+)\\)`, 'gi');
 
-  const mediaMin = [];
-  const mediaMax = [];
+      const mediaMin = [];
+      const mediaMax = [];
 
-  root.walkDecls((decl) => {
-    if (decl.value.indexOf(`${$opts.functionName}(`) === -1) {
-      return;
+      root.walkDecls((decl) => {
+        if (decl.value.indexOf(`${$opts.functionName}(`) === -1) {
+          return;
+        }
+
+        let min = decl.value.replace(regex, (_, values) => values.split(',').map((a) => a.trim())[0]);
+        let max = decl.value.replace(regex, (_, values) => values.split(',').map((a) => a.trim())[1]);
+
+        decl.value = decl.value.replace(regex, (_, values /*, index*/) => {
+          const { minVal, maxVal } = parseValues(values);
+
+          // TODO: Rem
+          // return `calc(${minValue} + ${maxValueInt} * (1px + ((100vw - ${$opts.min}) / (${p($opts.max)} - ${p($opts.min)})) - 1px))`;
+          // Px
+          return `calc(${minVal} + ${pf(maxVal) - pf(minVal)} * (100vw - ${$opts.min}) / ${pi($opts.max) - pi($opts.min)})`;
+        });
+
+        mediaMin.push({
+          selector: decl.parent.selector,
+          prop: decl.prop,
+          value: min,
+        });
+
+        mediaMax.push({
+          selector: decl.parent.selector,
+          prop: decl.prop,
+          value: max,
+        });
+      });
+
+      if (mediaMin.length) {
+        addMediaRule(root, `(max-width: ${$opts.min})`, mediaMin);
+      }
+
+      if (mediaMax.length) {
+        addMediaRule(root, `(min-width: ${$opts.max})`, mediaMax);
+      }
     }
-
-    let min = decl.value.replace(regex, (_, values) => values.split(',').map((a) => a.trim())[0]);
-    let max = decl.value.replace(regex, (_, values) => values.split(',').map((a) => a.trim())[1]);
-
-    decl.value = decl.value.replace(regex, (_, values /*, index*/) => {
-      const { minVal, maxVal } = parseValues(values);
-
-      // TODO: Rem
-      // return `calc(${minValue} + ${maxValueInt} * (1px + ((100vw - ${$opts.min}) / (${p($opts.max)} - ${p($opts.min)})) - 1px))`;
-      // Px
-      return `calc(${minVal} + ${pf(maxVal) - pf(minVal)} * (100vw - ${$opts.min}) / ${pi($opts.max) - pi($opts.min)})`;
-    });
-
-    mediaMin.push({
-      selector: decl.parent.selector,
-      prop: decl.prop,
-      value: min,
-    });
-
-    mediaMax.push({
-      selector: decl.parent.selector,
-      prop: decl.prop,
-      value: max,
-    });
-  });
-
-  if (mediaMin.length) {
-    addMediaRule(root, `(max-width: ${$opts.min})`, mediaMin);
   }
-
-  if (mediaMax.length) {
-    addMediaRule(root, `(min-width: ${$opts.max})`, mediaMax);
-  }
-});
+}
 
 function addMediaRule(root, params, children) {
   const m = postcss.atRule({ name: 'media', params });
